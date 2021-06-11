3047d2550c7bb0c412226861163c36024d596237
Mrinmoy Polley <82281251+Mrinmoy35@users.noreply.github.com>
2021-06-11 20:12:43 +0530

    Created swap no.c
    This file will print swapping of 2 numbers without using a temporary variable in c programming.

--- Swap no.c
+++ Swap no.c
@@ -4,9 +4,9 @@ int main()
 	int a=40, b=50;  
 	printf("Before swap a=%d b=%d",a,b); 
 	a=a+b;//a=90 (40+50)    
-    b=a-b;//b=40 (90-50)   
-    a=a-b;//a=50 (90-40)    
-    printf("\nAfter swap a=%d b=%d",a,b);    
-    return 0;
+        b=a-b;//b=40 (90-50)   
+        a=a-b;//a=50 (90-40)    
+        printf("\nAfter swap a=%d b=%d",a,b);    
+        return 0;
 	
 }
